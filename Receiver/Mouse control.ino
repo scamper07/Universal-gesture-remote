0f1f4331f0927ee7596528f9e45d4c8d96036d86
Preetham Kulai <preetham.kulai21@gmail.com>
2013-08-23 23:09:23 +0530

    Update Mouse control.ino

--- Receiver/Mouse control.ino
+++ Receiver/Mouse control.ino
@@ -1,6 +1,6 @@
-#include <IRremote.h>
+#include <IRremote.h>                   //IR remote library
 
-int RECV_PIN = 11;
+int RECV_PIN = 11;                      //pin to which IR receiver is connected
 
 IRrecv irrecv(RECV_PIN);
 
@@ -9,7 +9,7 @@ decode_results results;
 void setup()
 {
   Serial.begin(9600);
-  irrecv.enableIRIn(); // Start the receiver
+  irrecv.enableIRIn();                  // Start the receiver
 }
 
 void loop() {
@@ -17,7 +17,7 @@ void loop() {
     Serial.println(results.value, HEX);
     Serial.flush();
     delay(62);
-    irrecv.resume(); // Receive the next value
+    irrecv.resume();                    // Receive the next value
   }
-  Serial.flush(); // clear serial buffer
+  Serial.flush();                       // clear serial buffer
 }
